8acec4b9839d741154c2a50a98427ca7a60ccc08
Manasa-iota <nagarammanasa235@gmail.com>
2025-06-14 17:10:41 +0530

    handle upvote and onClick

--- src/components/feedback/FeedbackItem.tsx
+++ src/components/feedback/FeedbackItem.tsx
@@ -1,12 +1,23 @@
 import { TriangleUpIcon } from "@radix-ui/react-icons";
 import { type feedbackItemProp } from "../../lib/types";
+import { useState } from "react";
 
 export default function FeedbackItem({ upvoteCount, badgeLetter, company, text, daysAgo }: feedbackItemProp) {
+  const [toggleExpand,setToggleExand] = useState(false);
+  const [count,setCount] =useState(upvoteCount);
+
+  const handleUpvote = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>)=>{
+    e.stopPropagation(); 
+    e.currentTarget.disabled = true;
+    setCount(count+1);
+  }
+
   return (
-    <li className="feedback">
-        <button>
+    <li 
+      className={`feedback ${toggleExpand ? "feedback--expand" : ""}`} onClick={() => {setToggleExand(!toggleExpand);}}>
+        <button onClick={handleUpvote}>
           <TriangleUpIcon/>
-          <span>{upvoteCount}</span></button>
+          <span>{count}</span></button>
         <div>
           <p>{badgeLetter}</p>
         </div>
